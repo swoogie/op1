13cbc1afaf5eeaf9ad03673ea7dfe5d474a88b81
Ignas Biekša <ignasbieksa@gmail.com>
2022-03-11 22:59:27 +0200

    added golden ratio method

--- OM1.py
+++ OM1.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,7 +11,6 @@ def bisection(l, r, e):
     L = r - l
     xm = (l + r) / 2
 
-    plt.figure(num=0, dpi=120)
     ax = plt.gca()
     ax.set_ylim(-3, 50)
 
@@ -45,12 +45,52 @@ def bisection(l, r, e):
     plt.show()
     return xm
 
+
+def golden(l, r, e):
+    L = r - l
+    t = (-1 + math.sqrt(5)) / 2
+    x1 = r - t * L
+    x2 = l + t * L
+
+    ax = plt.gca()
+    ax.set_ylim(-3, 50)
+
+    x = np.linspace(l, r, num=100)
+    plt.plot(x, f(x), "k-")
+
+    while L >= e:
+
+        fx1 = f(x1)
+        fx2 = f(x2)
+
+        plt.plot(x1, fx1, "c|")
+        plt.plot(x2, fx2, "c|")
+        if fx2 < fx1:
+            l = x1
+            L = r - l
+            x1 = x2
+            x2 = l + t * L
+        else:
+            r = x2
+            L = r - l
+            x2 = x1
+            x1 = r - t * L
+
+    plt.show()
+    return x1
+
+
 def main():
     e = 0.00001
     l = 0
     r = 10
     minimum = bisection(l, r, e)
-    print ("minimum value: ", minimum)
+    print ("bisection results:\n",
+           "minimum value: ", minimum)
+
+    minimum = golden(l, r, e)
+    print ("golden ratio results:\n",
+           "minimum value: ", minimum)
 
 if __name__ == "__main__":
     main()
