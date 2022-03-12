48574ffbd54721104b4e377e5efe7164957deba3
Ignas Biekša <ignasbieksa@gmail.com>
2022-03-12 18:01:52 +0200

    added newton method

--- OM1.py
+++ OM1.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import sympy as sym
 import matplotlib.pyplot as plt
 
 
@@ -7,15 +8,19 @@ def f(x):
     return (x ** 2 - 4) ** 2 / 9 - 1
 
 
-def bisection(l, r, e):
-    L = r - l
-    xm = (l + r) / 2
-
+def plot(l, r):
     ax = plt.gca()
     ax.set_ylim(-3, 50)
 
     x = np.linspace(l, r, num=100)
     plt.plot(x, f(x), "k-")
+
+
+def bisection(l, r, e):
+    L = r - l
+    xm = (l + r) / 2
+
+    plot(l, r)
 
     while L >= e:
         x1 = l + L / 4
@@ -52,11 +57,7 @@ def golden(l, r, e):
     x1 = r - t * L
     x2 = l + t * L
 
-    ax = plt.gca()
-    ax.set_ylim(-3, 50)
-
-    x = np.linspace(l, r, num=100)
-    plt.plot(x, f(x), "k-")
+    plot(l, r)
 
     while L >= e:
 
@@ -80,6 +81,29 @@ def golden(l, r, e):
     return x1
 
 
+def newton(x0, e):
+    x = sym.Symbol('x')
+
+    df1 = sym.lambdify(x, sym.diff((x ** 2 - 4) ** 2 / 9 - 1, x))
+    df2 = sym.lambdify(x, sym.diff((x ** 2 - 4) ** 2 / 9 - 1, x, 2))
+
+    curr = x0 - df1(x0) / df2(x0)
+    prev = x0
+
+    plot(0, 10)
+
+    while abs(prev - curr) >= e:
+        plt.plot(prev, f(prev), "c|", markersize=15)
+        plt.plot(curr, f(curr), "c|", markersize=15)
+        prev = curr
+        curr = prev - df1(prev) / df2(prev)
+
+    plt.plot(curr, f(curr), "rx")
+    plt.show()
+
+    return curr
+
+
 def main():
     e = 0.00001
     l = 0
@@ -92,7 +116,10 @@ def main():
     print ("golden ratio results:\n",
            "minimum value: ", minimum)
 
+    minimum = newton(5, e)
+    print("golden ratio results:\n",
+          "minimum value: ", minimum)
+
+
 if __name__ == "__main__":
     main()
-
-    
